fb8a48cff3ba6cdb618c0fbeeb9cf2c7cb2a8236
Poren Chiang <ren.chiang@gmail.com>
2013-05-20 15:02:50 +0800

    Advanced index building script to ignore unsubstituted entries

--- build/build-index.py
+++ build/build-index.py
@@ -1,13 +1,22 @@
+# -*- coding: utf-8 -*-
 import json
-import os
 import codecs
+import re
 
 source = codecs.open('dict-revised.unicode.json', encoding='utf-8', mode='r')
 sourceJson = json.load(source)
 
 index = []
+ignore = [re.compile(x) for x in [r'\{\[[0-9a-f]{4}\]\}', u'\uDB40[\uDD00-\uDD0F]', r'[⿰⿸]']]
+
+def ignoreEntry(title):
+	for expr in ignore:
+		if expr.search(title): return True
+	return False
+	
 for entry in sourceJson:
+	if ignoreEntry(entry['title']): continue
 	index.append(entry['title'])
 
 target = codecs.open('../data/index.json', encoding='utf-8', mode='w+')
-json.dump(index, target)
+json.dump(index, target, ensure_ascii=False, indent=0)
