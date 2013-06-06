a7510e4f66616fb1e5d33a7836354fa9740ff3ae
Poren Chiang <ren.chiang@gmail.com>
2013-06-07 07:40:50 +0800

    Finished updater implementation

--- moedict-meego/qml/updater.js
+++ moedict-meego/qml/updater.js
@@ -10,13 +10,12 @@ function loadDict(message) {
     db.transaction(function(tx) {
                        var count = 0
                        for (var key in dictObj) {
-                           tx.executeSql("REPLACE INTO entries (title,json) VALUES (?,?)", [key, dictObj[key]])
+                           tx.executeSql("REPLACE INTO entries(title,json) VALUES (?,?)", [key, dictObj[key]])
 
                            // Use shifting operator to increase performance
                            // Estimate size: 2048 * 80 = 163840 (v13.0429 has 162179)
-                           // Progress: 0 - 80%
                            if (((++count) & 2047) === 0)
-                               WorkerScript.sendMessage({"progress": (count >> 11) / 100 })
+                               WorkerScript.sendMessage({"progress": (count >> 11) / 160 })
                        }
                    })
     dictObj = undefined
@@ -25,6 +24,26 @@ function loadDict(message) {
     var indexObj = JSON.parse(message.index)
     message.index = undefined
     gc()
+
+    db.transaction(function(tx) {
+                       var count = 0
+                       for (var indexType in indexObj) {
+                           var indexDict = indexObj[indexType]
+                           for (var key in indexDict) {
+                               for (var title in indexDict[key])
+                                   tx.executeSql("REPLACE INTO indices(key,title) VALUES (?,?)", [key, title])
+
+                               // Estimate size: 4096 * 67 = 274432 (v13.0429 has 277174)
+                               if (((++count) & 4095) === 0)
+                                   WorkerScript.sendMessage({"progress": 0.5 + (count >> 12) / 67})
+                           }
+                       }
+                   })
+
+    indexObj = undefined
+    gc()
+
+    WorkerScript.sendMessage({"completed": true})
 }
 
 WorkerScript.onMessage = loadDict
