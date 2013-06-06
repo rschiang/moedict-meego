001bd9cb78db4a44b46ea5bc6ee69be11a203e00
Poren Chiang <ren.chiang@gmail.com>
2013-06-07 07:29:32 +0800

    Implementing parser

--- moedict-meego/qml/updater.js
+++ moedict-meego/qml/updater.js
@@ -2,7 +2,29 @@
 // This thread parses and loads dictionary JSON into database
 
 function loadDict(message) {
+    var dictObj = JSON.parse(message.dict)
+    message.dict = undefined
+    gc()
 
+    var db = openDatabaseSync("org.moedict", "1", "", 64 * 1024 * 1024)
+    db.transaction(function(tx) {
+                       var count = 0
+                       for (var key in dictObj) {
+                           tx.executeSql("REPLACE INTO entries (title,json) VALUES (?,?)", [key, dictObj[key]])
+
+                           // Use shifting operator to increase performance
+                           // Estimate size: 2048 * 80 = 163840 (v13.0429 has 162179)
+                           // Progress: 0 - 80%
+                           if (((++count) & 2047) === 0)
+                               WorkerScript.sendMessage({"progress": (count >> 11) / 100 })
+                       }
+                   })
+    dictObj = undefined
+    gc()
+
+    var indexObj = JSON.parse(message.index)
+    message.index = undefined
+    gc()
 }
 
 WorkerScript.onMessage = loadDict
